2b48720dfc5692c940cb1e5b29f9ab16f12b7d16
Dimitrije Dragasevic <dimitrijedragasevic@Savas-MacBook-Pro.local>
2023-07-27 15:03:06 +0200

    fix veriable

--- pages/terrastation/create-wallet-page.js
+++ pages/terrastation/create-wallet-page.js
@@ -4,7 +4,7 @@ const inputconfirmPassword = '[name="confirm"]'
 const mnemonicText = '[class="TextArea_textarea__2a4Ez"]'
 const checkBox = '[class="Checkbox_track__2gz9s"]'
 const submitButton = '[type="submit"]'
-mnemonicNumber = '[class="Form_label__bOv6h"]'
+const mnemonicNumber = '[class="Form_label__bOv6h"]'
 
 
 module.exports.createWalletElements = {
